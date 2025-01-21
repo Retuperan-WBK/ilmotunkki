1fae5f5ca2f59a5d9a50978de6146898cbed523f
miikkalaitinen <38249083+miikkalaitinen@users.noreply.github.com>
2025-01-21 22:28:22 +0200

    fix new orders showing in groups

--- web/app/api/admin/groups/route.ts
+++ web/app/api/admin/groups/route.ts
@@ -33,10 +33,20 @@ export const GET = async (req: NextRequest) => {
     }, token.value);
 
     // Filter out groups without orders in the 'ok' or 'admin-new' status
-
     const filteredGroups = groups.filter((group) => group.attributes.orders?.data.some((order) => order.attributes.status === "ok" || order.attributes.status === "admin-new"));
 
-    return NextResponse.json(filteredGroups, { status: 200 });
+    // Filter out orders that are not 'ok' or 'admin-new' status from each group
+    const groupsFiltered = filteredGroups.map((group) => {
+      return {
+        ...group,
+        attributes: {
+          ...group.attributes,
+          orders: group.attributes.orders?.data.filter((order) => order.attributes.status === "ok" || order.attributes.status === "admin-new"),
+        },
+      };
+    });
+
+    return NextResponse.json(groupsFiltered, { status: 200 });
   } catch (error) {
     console.error("Error fetching groups:", error);
     return NextResponse.json({ error: "Internal server error" }, { status: 500 });
